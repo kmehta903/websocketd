dd5cf3d7a6d6606abfd3ceb79252240f1bc3cf02
Krisha Mehta <krisha.mehta3@gmail.com>
2018-05-21 18:11:33 -0500

    update solution to eliminate need for output file

--- examples/bash/README.txt
+++ examples/bash/README.txt
@@ -5,5 +5,3 @@ websocketd --port=8080 --staticdir=. ./RandomData.sh
 
 Navigate to the following in the browser:
 http://localhost:8080/Canvas.html
-
-NOTE: Due to inherent entropy issues, data may not always be random each time "Fill Canvas" button is clicked
--- examples/bash/RandomData.sh
+++ examples/bash/RandomData.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Simple script that generates 10kb or 80,000 bits of random data to output.bin file
+# Simple script that outputs 10kb or 10,000 bytes of random data from websocket to DOM
 
 for n in {1..1}; do
-
-dd if=/dev/random of=output.bin bs=10000 count=1
+# head -c 100000 /dev/urandom: Obtain the first 100000 characters (bytes) from /dev/urandom.
+# tr -dc 'a-zA-Z0-9~!@#$%^&*_-': Remove all but the characters specified in 'a-zA-Z0-9~!@#$%^&*_-' from the output of the first command.
+# head -c 10000: Format the output result to 10000 characters (bytes) on stdout
+rand=$(head -c 100000 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | head -c 10000)
+echo $rand
 
 done
